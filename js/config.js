3d5d370c6741611903ce24a45afed608fd31075a
VINIT BANSAL <98345312+Vinit-bansal@users.noreply.github.com>
2026-07-19 11:42:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Your deployed Apps Script Web App URL.
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz5zXFAruUfPOQNAc5CK0-flFb0mMSZxkgi_5KEBtIt2mLbMnR3Mu1LjvPBaRZZvm5M/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxIFBw3vv_bAHdiJguILcsOkbo-WBbH-QP0x6tLawLsERQsr9HwZL-tAKkHr2JttgM/exec";
