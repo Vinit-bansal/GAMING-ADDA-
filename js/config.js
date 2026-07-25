c1aea16855abb45187780ae7f946748512a9355b
VINIT BANSAL <98345312+Vinit-bansal@users.noreply.github.com>
2026-07-25 13:18:33 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,2 @@
+// Your deployed Apps Script Web App URL.
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz5zXFAruUfPOQNAc5CK0-flFb0mMSZxkgi_5KEBtIt2mLbMnR3Mu1LjvPBaRZZvm5M/exec";
